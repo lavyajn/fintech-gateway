46c7c5d5a4208e2ebb756ca2d55165e7fdd0bbde
Lavya <lavyajain.dev@gmail.com>
2025-08-29 01:50:25 +0530

    Server setup!

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const dotenv = require ('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    res.send('Payment Gateway is alive!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running is ${PORT}`);
+});
+
